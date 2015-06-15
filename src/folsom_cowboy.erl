677ded0d52cf14ce04f52ad11ff0c73e829f8fb4
Evan Vigil-McClanahan <evan@heroku.com>
2015-06-15 16:08:42 -0700

    sanitize encode inputs
    JSX is less accepting than mochijson, so we need to make sure that we're
    not feeding it bad inputs

--- src/folsom_cowboy.erl
+++ src/folsom_cowboy.erl
@@ -8,8 +8,9 @@ dispatch() ->
      {"/_metrics", folsom_cowboy_handler, [metrics]},
      {"/_metrics/[:metric_id]", folsom_cowboy_handler, [metric]},
      {"/_ping", folsom_cowboy_handler, [ping]},
-     {"/_port", folsom_cowboy_handler, [port]},
-     {"/_process", folsom_cowboy_handler, [process]},
+     %% the below are hideously unsafe!
+     %% {"/_port", folsom_cowboy_handler, [port]},
+     %% {"/_process", folsom_cowboy_handler, [process]},
      {"/_statistics", folsom_cowboy_handler, [statistics]},
      {"/_system", folsom_cowboy_handler, [system]},
      {"/_ets", folsom_cowboy_handler, [ets]},
